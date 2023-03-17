8dd44a4f722fcca02cb7656dc2a49cd89ebc0bbd
Sean Wong <seanwong24@live.com>
2023-03-17 01:31:23 +0000

    use multiple entries for building

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
+import {defineConfig} from 'vite';
+import glob from 'tiny-glob';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
+      entry: await glob('src/**/!(*.style|*.d).ts'),
+      formats: ['es'],
     },
     rollupOptions: {
       // external: /^lit/,
     },
   },
-  base: "./",
+  base: './',
 });
